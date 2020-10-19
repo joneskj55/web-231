a4073a532fe64ce18db7d0abbf31f5a7c9a16f04
joneskj55 <joneskj55@gmail.com>
2020-10-19 15:09:45 -0500

    assignment 1.4

---
+++ week-1/jones-assignment1.4.js
@@ -0,0 +1,13 @@
+var firstName = 'Kevin';
+var lastName = 'Jones';
+var age = 36;
+var street = '123 Notarealstreet Ave.'
+var city = 'Dushanbe';
+var zipCode = 12345;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
