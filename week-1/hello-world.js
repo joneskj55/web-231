99cb258ec0b36f2a6019138b2d44a9b2ab3228b3
joneskj55 <joneskj55@gmail.com>
2020-10-20 07:51:12 -0500

    add attribution

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,11 @@
+/* 
+========================================== 
+; Title: Exercise 1.3
+; Author: Kevin Jones 
+; Date: 19 October 2020 
+; Modified By: Kevin Jones 
+; Description: Hello World assignment.
+; ======================================== 
+*/
+
 console.log('Hello World, my name is Kevin');
