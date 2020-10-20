6b1992355f988392bce6f5fd3bc9207c5547edb3
joneskj55 <joneskj55@gmail.com>
2020-10-20 07:33:45 -0500

    add attribution

--- week-1/jones-assignment1.4.js
+++ week-1/jones-assignment1.4.js
@@ -1,3 +1,15 @@
+/* 
+========================================== 
+; Title: Assignment 1.4
+; Author: Kevin Jones 
+; Date: 19 October 2020 
+; Modified By: Kevin Jones 
+; Description: This is the Primitive
+; Types assignment.
+; ======================================== 
+*/
+
+// variables
 var firstName = 'Kevin';
 var lastName = 'Jones';
 var age = 36;
@@ -5,6 +17,7 @@ var street = '123 Notarealstreet Ave.'
 var city = 'Dushanbe';
 var zipCode = 12345;
 
+// logs
 console.log(firstName);
 console.log(lastName);
 console.log(age);
