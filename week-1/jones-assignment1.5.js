df03a45b3a5b55d101414b92773786b855f2df23
joneskj55 <joneskj55@gmail.com>
2020-10-21 11:30:55 -0500

    1.5 initial

---
+++ week-1/jones-assignment1.5.js
@@ -0,0 +1,19 @@
+/* 
+========================================== 
+; Title: Assignment 1.5
+; Author: Kevin Jones 
+; Date: 20 October 2020 
+; Modified By: Kevin Jones 
+; Description: This is the Types,
+; Values, and Variables assignment.
+; ======================================== 
+*/
+
+// employee one
+var firstName = 'Larry';
+var lastName = 'David';
+var address = '123 Baker St.';
+var payRate = 65.39;
+console.log(payRate.toFixed(1));
+var hireDate = new Date(2016, 4, 15);
+console.log(hireDate.toLocaleDateString());
