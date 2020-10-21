ec0c7e9f11d0700a7ecbbc0df2143629f6272805
joneskj55 <joneskj55@gmail.com>
2020-10-21 15:24:22 -0500

    add formatting

--- week-1/jones-assignment1.5.js
+++ week-1/jones-assignment1.5.js
@@ -9,11 +9,50 @@
 ; ======================================== 
 */
 
-// employee one
+// employee 1
 var firstName = 'Larry';
 var lastName = 'David';
 var address = '123 Baker St.';
 var payRate = 65.39;
-console.log(payRate.toFixed(1));
 var hireDate = new Date(2016, 4, 15);
-console.log(hireDate.toLocaleDateString());
+
+// employee 1 output
+console.log('First Name: ' + firstName);
+console.log('Last Name: ' + lastName);
+console.log('Address: ' + address);
+console.log('Pay Rate: ' + payRate.toFixed(1));
+console.log('Hire Date: ' + hireDate.toLocaleDateString());
+
+// space
+console.log("");
+
+// employee 2
+var firstName = 'Jimmy';
+var lastName = 'Dean';
+var address = '452 Addison Ave.';
+var payRate = 72.54;
+var hireDate = new Date(2012, 6, 20);
+
+// employee 2 output
+console.log('First Name: ' + firstName);
+console.log('Last Name: ' + lastName);
+console.log('Address: ' + address);
+console.log('Pay Rate: ' + payRate.toFixed(1));
+console.log('Hire Date: ' + hireDate.toLocaleDateString());
+
+// space
+console.log("");
+
+// employee 3
+var firstName = 'Jimbob';
+var lastName = 'Cooter';
+var address = '512 McPhearson St.';
+var payRate = 36.45;
+var hireDate = new Date(2019, 2, 25);
+
+// employee 3 output
+console.log('First Name: ' + firstName);
+console.log('Last Name: ' + lastName);
+console.log('Address: ' + address);
+console.log('Pay Rate: ' + payRate.toFixed(1));
+console.log('Hire Date: ' + hireDate.toLocaleDateString());
