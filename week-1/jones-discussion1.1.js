979c1350375186d7e6881bd55f0575a7babe81f1
joneskj55 <joneskj55@gmail.com>
2020-10-23 20:31:31 -0500

    add discussion 1.1

---
+++ week-1/jones-discussion1.1.js
@@ -0,0 +1,21 @@
+/* 
+========================================== 
+; Title: Discussion 1.1
+; Author: Kevin Jones 
+; Date: 19 October 2020 
+; Modified By: Kevin Jones 
+; Description: JavaScript program with
+; two errors.
+; ======================================== 
+*/
+
+// no need for comments here
+var case = 'hmm';
+var cheese = 'can't do this either';
+
+function whyWontThisWork() {
+    console.log(case);
+    return cheese;
+}
+
+whyWontThisWork();
