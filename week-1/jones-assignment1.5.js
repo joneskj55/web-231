a6ea89ce56a63f17eb9c0e6aba817f08daaacbb0
joneskj55 <joneskj55@gmail.com>
2020-10-27 17:16:22 -0500

    update variables

--- week-1/jones-assignment1.5.js
+++ week-1/jones-assignment1.5.js
@@ -1,58 +1,58 @@
-/* 
-========================================== 
+/*
+==========================================
 ; Title: Assignment 1.5
-; Author: Kevin Jones 
-; Date: 20 October 2020 
-; Modified By: Kevin Jones 
+; Author: Kevin Jones
+; Date: 20 October 2020
+; Modified By: Kevin Jones
 ; Description: This is the Types,
 ; Values, and Variables assignment.
-; ======================================== 
+; ========================================
 */
 
 // employee 1
-var firstName = 'Larry';
-var lastName = 'David';
-var address = '123 Baker St.';
+var firstName = "Larry";
+var lastName = "David";
+var address = "123 Baker St.";
 var payRate = 65.39;
 var hireDate = new Date(2016, 4, 15);
 
 // employee 1 output
-console.log('First Name: ' + firstName);
-console.log('Last Name: ' + lastName);
-console.log('Address: ' + address);
-console.log('Pay Rate: ' + payRate.toFixed(1));
-console.log('Hire Date: ' + hireDate.toLocaleDateString());
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Address: " + address);
+console.log("Pay Rate: " + payRate.toFixed(1));
+console.log("Hire Date: " + hireDate.toLocaleDateString());
 
 // space
 console.log("");
 
 // employee 2
-var firstName = 'Jimmy';
-var lastName = 'Dean';
-var address = '452 Addison Ave.';
-var payRate = 72.54;
-var hireDate = new Date(2012, 6, 20);
+var firstName2 = "Jimmy";
+var lastName2 = "Dean";
+var address2 = "452 Addison Ave.";
+var payRate2 = 72.54;
+var hireDate2 = new Date(2012, 6, 20);
 
 // employee 2 output
-console.log('First Name: ' + firstName);
-console.log('Last Name: ' + lastName);
-console.log('Address: ' + address);
-console.log('Pay Rate: ' + payRate.toFixed(1));
-console.log('Hire Date: ' + hireDate.toLocaleDateString());
+console.log("First Name: " + firstName2);
+console.log("Last Name: " + lastName2);
+console.log("Address: " + address2);
+console.log("Pay Rate: " + payRate2.toFixed(1));
+console.log("Hire Date: " + hireDate2.toLocaleDateString());
 
 // space
 console.log("");
 
 // employee 3
-var firstName = 'Jimbob';
-var lastName = 'Cooter';
-var address = '512 McPhearson St.';
-var payRate = 36.45;
-var hireDate = new Date(2019, 2, 25);
+var firstName3 = "Jimbob";
+var lastName3 = "Cooter";
+var address3 = "512 McPhearson St.";
+var payRate3 = 36.45;
+var hireDate3 = new Date(2019, 2, 25);
 
 // employee 3 output
-console.log('First Name: ' + firstName);
-console.log('Last Name: ' + lastName);
-console.log('Address: ' + address);
-console.log('Pay Rate: ' + payRate.toFixed(1));
-console.log('Hire Date: ' + hireDate.toLocaleDateString());
+console.log("First Name: " + firstName3);
+console.log("Last Name: " + lastName3);
+console.log("Address: " + address3);
+console.log("Pay Rate: " + payRate3.toFixed(1));
+console.log("Hire Date: " + hireDate3.toLocaleDateString());
