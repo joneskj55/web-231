b8168ca397dbd70b4469096dcd008ac3a31d8d0a
joneskj55 <joneskj55@gmail.com>
2020-11-02 10:56:56 -0600

    add discussion 3.1

---
+++ week-3/jones-discussion3.1.js
@@ -0,0 +1,32 @@
+/*
+============================================
+; Title:  Discussion Board 3.1
+; (Control Statements)
+; Author: Kevin Jones
+; Date: 2 November 2020
+; Description: Control statement program
+; with at least two errors
+;===========================================
+*/
+
+// declare variables
+let correct = "This is correct.";
+let incorrect = "This is incorrect.";
+
+// switch statement
+switch (1 + 1) {
+  case 2:
+    console.log(correct);
+    break:
+  case 4:
+    console.log(incorrect);
+    break;
+  case 6:
+    console.log(incorrect);
+    break;
+  case 8:
+    console.log(incorrect);
+    break;
+  default;
+    console.log("???");
+}
