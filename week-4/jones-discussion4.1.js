1160e1c5aac9bc27102cb4b02aa5d0cef3e0d90a
joneskj55 <joneskj55@gmail.com>
2020-11-09 09:26:09 -0600

    discussion 4.1

---
+++ week-4/jones-discussion4.1.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title:  Discussion Board 4.1 - Arrays
+; Author: Kevin Jones
+; Date: 9 November 2020
+; Description: JavaScript Array program
+; with at least two errors
+;===========================================
+*/
+
+// ---------- This program should log: 1, three, 5, seven, 9, seven ---------- //
+
+// declare array variable
+let myArray = [1, "three", 5, seven];
+
+// function to manipulate array
+function playWithArray() {
+  myArray.pop(9);
+}
+
+// invoke function
+playWithArray();
+
+// display output
+console.log(myArray + "," + myArray[4]); // should return [ 1, three, 5, seven, 9, seven ]
